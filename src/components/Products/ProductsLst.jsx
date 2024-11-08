957a2bdc070b9d0890cd3b359b6ef6b477629be8
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-11-08 23:46:01 +0300

    ürün markalarına filtreleme yapılması

--- src/components/Products/ProductsLst.jsx
+++ src/components/Products/ProductsLst.jsx
@@ -9,6 +9,7 @@ import {
   CardText,
   CardHeader,
   CardFooter,
+  FormSelect,
 } from "react-bootstrap";
 import { IoBagAdd } from "react-icons/io5";
 import { useEffect } from "react";
@@ -16,11 +17,45 @@ import { useDispatch } from "react-redux";
 import { getAllProducts } from "../../redux/slices/productSlice";
 import "./Products.scss";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+// Fiyatları türk tl karşılık formatlama
+const formatPrice = (price) => {
+  return new Intl.NumberFormat("tr-TR", {
+    style: "currency",
+    currency: "TRY",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(price);
+};
+
+const knownBrands = ["Lenovo", "Asus", "Hp", "Acer", "Dell", "Samsung"];
 
 function ProductsLst() {
+  const [selectedBrand, setSelectedBrand] = useState("Hepsi");
   const { products, loading } = useSelector((store) => store.products);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  // Burda ise bu fonksiyn dışardan title alıp bizim ürünün başlıgı içerisinde bizim hazırladıgımız ürünlerin başlıkları içeriyorsa bunu deger dönüyor
+  const extractBrand = (title) => {
+    const foundBrand = knownBrands.find((brand) => title.includes(brand));
+    return foundBrand || "Hepsi";
+  };
+
+  // Burda ise ürünlerimiz üzerinde filtreleme yapıyoruz. Seçenek hepsi ise veya bizim extrBrn gelcek olan title degeri seçilen seçenege eşit ise filtreleme çalışıyor
+  const filteredProducts = products.filter((product) => {
+    return (
+      selectedBrand === "Hepsi" || extractBrand(product.title) === selectedBrand
+    );
+  });
+
+  // Bütün ürünleri gezerek extractBranda title degerini parametre gönderdik
+  const uniqueBrands = [
+    "Hepsi",
+    ...new Set(products.map((product) => extractBrand(product.title))),
+  ];
 
   useEffect(() => {
     dispatch(getAllProducts());
@@ -40,9 +75,21 @@ function ProductsLst() {
 
   return (
     <div>
+      <div className="text-center">
+        <FormSelect
+          value={selectedBrand}
+          onChange={(e) => setSelectedBrand(e.target.value)}
+        >
+          {uniqueBrands.map((item, index) => (
+            <option key={index} value={item}>
+              {item}
+            </option>
+          ))}
+        </FormSelect>
+      </div>
       <Container>
         <Row>
-          {products.map((item) => (
+          {filteredProducts.map((item) => (
             <Col
               lg="4"
               md="4"
@@ -61,12 +108,12 @@ function ProductsLst() {
                 />
                 <CardBody className="card-body">
                   <CardText className="card-text-price">{item.title}</CardText>
-                  <CardText className="card-text fw-bold ">
-                    {item.price.toFixed(3)}$
+                  <CardText className="card-text fw-bold">
+                    {formatPrice(item.price)}
                   </CardText>
                 </CardBody>
                 {item.seller && (
-                  <CardFooter className="fw-light card-footer">
+                  <CardFooter className="fw-light card-footer ">
                     <p className="seller-text fw-bold">
                       Satıcı:
                       <p className="text-muted fw-medium">
