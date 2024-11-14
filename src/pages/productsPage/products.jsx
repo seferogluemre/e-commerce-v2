ff276173e6bc7b0bcd4299e614b3eb1ec9ffa71b
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-11-15 01:57:49 +0300

    düzenlemeler

--- src/pages/productsPage/products.jsx
+++ src/pages/productsPage/products.jsx
@@ -44,7 +44,15 @@ function Product() {
   }, [category]);
 
   if (loading) {
-    return <p>ahsahshahs</p>;
+    return (
+      <div className="loader-container">
+        <div className="bouncing-dots">
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+        </div>
+      </div>
+    );
   }
 
   return (
